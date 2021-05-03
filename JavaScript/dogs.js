41f188e4dd42248107a4ca33d187a7d9fdac43eb
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-05-03 00:28:10 -0500

    cambios nuevos

--- JavaScript/dogs.js
+++ JavaScript/dogs.js
@@ -1,17 +1,6 @@
 'use strict'
 
-class mascota{
-    constructor(categoria,nombre,raza){
-        this.categoria = categoria;
-        this.nombre = nombre;
-        this.raza = raza;
-    }
-}
 
-const perros = [];
-perros.push(new mascota('perro', 'Chester', 'Golden retriever'), new mascota('perro', 'Papi', 'Chihuahua'), new mascota('perro', 'Rocky', 'Rottweiler'), new mascota('perro', 'Pelusa', 'Bichón frisé'));
-
-const imagenes_perros = ['/imagenes/chester.svg', '/imagenes/papi.svg', '/imagenes/rocky.svg','/imagenes/pelusa.svg']
 
 // console.log(imagenes_perros);
 // let filtracion = perros.filter((valor) => {
@@ -34,16 +23,5 @@ const imagenes_perros = ['/imagenes/chester.svg', '/imagenes/papi.svg', '/imagen
 let contenedor_perros1 = document.querySelector('.perros1');
 let contenedor_perros2 = document.querySelector('.perros2');
 
-imagenes_perros.forEach((perro,index) => {
-    if(index == 0 || index == 2){
-        contenedor_perros1.innerHTML += `
-        <img src = "${perro}" class = "${perros[index].nombre}">
-        `;
-    }
-    else if(index == 1 || index == 3){
-        contenedor_perros2.innerHTML += `
-        <img src = "${perro}" class = "${perros[index].nombre}">  `
-    }
-});
 
 
--- JavaScript/home.js
+++ JavaScript/home.js
@@ -1,4 +1,16 @@
 'use strict'
+class mascota{
+    constructor(categoria,nombre,raza){
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+}
+
+const perros = [];
+perros.push(new mascota('perro', 'Chester', 'Golden retriever'), new mascota('perro', 'Papi', 'Chihuahua'), new mascota('perro', 'Rocky', 'Rottweiler'), new mascota('perro', 'Pelusa', 'Bichón frisé'));
+const gatos = []
+gatos.push(new mascota('gato', 'Matilde', 'British Shorthair'), new mascota('gato', 'Pelusa', 'Birmano'), new mascota('gato', 'Kity', 'Bombay'), new mascota('gato', 'Bombon', 'Gato Americano'));
 
 // Funciones para opacidad de botones
 
@@ -16,6 +28,43 @@ const dispararOpacidadPerro = () => {
     dog_boton.classList.add('.opacity_activa');
 }    
 
+const dibujarPerros = () =>{
+    // let contenedor_gatos = document.querySelector('.cards_gatos');
+    // contenedor_gatos.style.display = 'none';
+    let contenedor = document.querySelector('.cards_perros');
+    // contenedor.style.display = 'flex';
+    imagenes_perros.forEach((perro,index) => {
+        if(index == 0 || index == 2){
+            contenedor.innerHTML += `
+            <img src = "${perro}" class = "${perros[index].nombre}">
+            `;
+        }
+        else if(index == 1 || index == 3){
+            contenedor.innerHTML += `
+            <img src = "${perro}" class = "${perros[index].nombre}">  `
+        }
+    });
+}
+
+// const dibujarGatos = () =>{
+//     let contenedor_perros = document.querySelector('.cards_perros');
+//     contenedor_perros.style.display = 'none';
+//     let contenedor = document.querySelector('.cards_gatos');
+//     contenedor.style.display = 'flex'
+//     imagenes_gatos.forEach((gato,index) => {
+//         if(index == 0 || index == 2){
+//             contenedor.innerHTML += `
+//             <img src = "${gato}" class = "${gatos[index].nombre}">
+//             `;
+//         }
+//         else if(index == 1 || index == 3){
+//             contenedor.innerHTML += `
+//             <img src = "${gato}" class = "${gatos[index].nombre}">  `
+//         }
+//     });
+// }
+
+
 // Botones con opacity
 
 const dog_boton = document.querySelector('.dog_icon');
@@ -24,12 +73,26 @@ const cat_boton = document.querySelector('.cat_icon');
 cat_boton.addEventListener("click", dispararOpacidadGato);
 dog_boton.addEventListener("click", dispararOpacidadPerro);
 
-let direccion = '/imagenes/chester.svg';
 
-let contenedor = document.querySelector('.prueba1');
-contenedor.innerHTML += `
-    <img  src = "${direccion}">
-    <img  src = "${direccion}">
-    <img  src = "${direccion}">
-    <img  src = "${direccion}">
-`;
+const imagenes_perros = ['/imagenes/chester.svg', '/imagenes/papi.svg', '/imagenes/rocky.svg','/imagenes/pelusa.svg']
+
+const imagenes_gatos = ['/imagenes/matilde.png', '/imagenes/pelusa.png', '/imagenes/kity.png', '/imagenes/bombon.png'];
+const cards_perros = document.querySelector('#cards-perros');
+cards_perros.addEventListener("click", dibujarPerros);
+
+
+
+
+//Prueba de remover 
+
+for(var i = 0; i < perros.length; i++){
+    var pruebam = document.querySelector();
+}
+
+
+// function remover (){
+//     document.body.removeChild(pruebam);
+// }
+
+// const cards_gatos = document.querySelector('#cards-gatos');
+// cards_gatos.addEventListener("click",remover);
